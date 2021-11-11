253e2f2feacbd0873c5540ae38d488430948d36d
lodex23 <81414802+lodex23@users.noreply.github.com>
2021-11-11 20:54:17 +0100

    Update nmapper.py

--- nmapper.py
+++ nmapper.py
@@ -5,21 +5,45 @@ os.system('clear')
 print("-----------------------------")
 print("Target Ip set to: " + ip)
 print("-----------------------------")
-input("Press enter to start scan")
-
-
-os.system('clear')
-os.system('nmap -sV ' + ip + ' -o nmap_result.txt')
-print("-----------------------------")
-input("Scane done: Press Enter to show Results")
-
-
-print("-----------------------------")
 
 os.system('clear')
 
-f = open("nmap_result.txt", "r")
-print(f.read())
 
 
-os.system('rm nmap_result.txt')
+def Function_IP_protocol(host):
+    os.system('nmap -sO ' + host)
+
+
+def Function_Service_Version(host):
+    os.system('nmap -sV ' + host)
+
+
+def Function_All_Hosts(host):
+    os.system('nmap -sO ' + host + ' -Pn')
+
+
+
+print("--------------------------------")
+print("Choose option")
+print("--------------------------------")
+print("1) Ip Protocol")
+print("2) Service/Version")
+print("3) All Hosts")
+print("--------------------------------")
+
+oP = input("Choose option: ")
+
+
+if oP == 1:
+    Function_IP_protocol(ip)
+
+elif oP == 2:
+    Function_Service_Version(ip)
+
+elif oP == 3:
+    Function_All_Hosts(ip)
+
+else:
+    print("This Number is invalid.")
+
+    
